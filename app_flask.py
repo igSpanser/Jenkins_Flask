6be7de74ab70616f4b7435036f1d019826ed4a0a
igSpanser <156592187+igSpanser@users.noreply.github.com>
2024-04-14 15:13:50 +0530

    updated port

--- app_flask.py
+++ app_flask.py
@@ -10,6 +10,6 @@ def hello():
     return "Hello World"
 
 if (__name__ == "__main__"):
-    app.run(host='0.0.0.0',port='8080', debu=True)
+    app.run(host='0.0.0.0',port='5000', debu=True)
 
 ##### Copy IP from terminal output and put it it web browser. 
